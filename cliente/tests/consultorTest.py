e910e670e72c270003158fa19f8b4610089b5ade
mboscovich <mboscovich@users.noreply.github.com>
2011-03-06 20:01:55 +0000

    ***

--- cliente/tests/consultorTest.py
+++ cliente/tests/consultorTest.py
@@ -7,7 +7,20 @@ import sys,  unittest
 sys.path.append('../clases')
 import consultor
 
+class Urls(unittest.TestCase):
+    extensiones_exceptuadas=("gif","jpeg","jpg","png","js","css","swf","ico","json","mp3","wav","rss","rar","zip","pdf","xml")      
+    extensiones_no_exceptuadas=("html","htm","txt")  
+    consult=consultor.Consultor()
+    
+    def verificarExtensionesExceptuadas(self):
+        """Verificar que se exceptuen las extensiones no analizables por dansguardian"""
+        for extension in self.extensiones_exceptuadas:
+            self.assertTrue(consult.extensionValida(url))
+             
+    def verificarExtensionesNoExceptuadas(self):
+        """Verificar que NO se exceptuen las extensiones analizables por dansguardian"""
+        for extension in self.extensiones_no_exceptuadas:
+            self.assertFalse(consultor.Consultor.extensionValida(url))
 
-            
 if __name__ == '__main__':
     unittest.main()
