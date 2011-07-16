687f03a0ccc9d1fa56ef0b186d91825b9da40eca
mboscovich <mboscovich@users.noreply.github.com>
2011-07-16 18:48:01 +0000

    agrego el filtrado por dns

--- cliente/conf/config.py
+++ cliente/conf/config.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import platform, os, _winreg
+import platform, os
 
 if  platform.uname()[0] == 'Linux':
     PATH_DB='/var/cache/kerberus/kerberus.db'
     LOG_FILENAME='/var/log/kerberus-cliente.log'    
 else:
+    import _winreg
     key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r'Software\kerberus')
     PATH_DB=_winreg.QueryValueEx(key,'kerberus-common')[0]+'\kerberus.db' 
     LOG_FILENAME=_winreg.QueryValueEx(key,'kerberus-common')[0]+'\kerberus-cliente.log'
