d97057ab4fbdbdf06426d065e8f0c95e5ae68cce
mboscovich <mboscovich@users.noreply.github.com>
2012-09-29 23:40:42 +0000

    termino la parte del desinstalador de linux. Ahora pide pass antes de desinstalar

--- cliente/branches/release/1.1-rev765/TODO.py
+++ cliente/branches/release/1.1-rev765/TODO.py
@@ -1,9 +1,8 @@
-* Hacer que thunderbird utilice el proxy (al menos en linux no se setea).
-* Hacer actualizador para linux.
-* Hacer que el desinstalador de linux notifique la desinstalacion
-* Internacionalizacion
-* hacer que se descargue desde el servidor la lista de servidores disponibles.
-* utilizar HTTPS
-* La pagina de sitio denegado tiene que estar en el server, no en el cliente.
-* Ni bien se cambie la contraseña se deberia notificar.
+# * utilizar HTTPS
+# * hacer que se descargue desde el servidor la lista de servidores disponibles
+# * La pagina de sitio denegado tiene que estar en el server, no en el cliente.
+# * Ni bien se cambie la contraseña se deberia notificar.
+# * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
+# * Hacer actualizador para linux.
+# * Internacionalizacion
 
