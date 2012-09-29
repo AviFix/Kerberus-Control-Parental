3014a84678f7c41fd1b06dca672163bdbaa572f9
mboscovich <mboscovich@users.noreply.github.com>
2012-09-29 23:50:59 +0000

    ahora se informa la cantidad de usuarios, y se sacan las '' cuando los datos los agrega el cluster

--- cliente/branches/release/1.1-rev765/TODO.py
+++ cliente/branches/release/1.1-rev765/TODO.py
@@ -1,7 +1,7 @@
+# * Ni bien se cambie la contraseña se deberia notificar.
 # * utilizar HTTPS
 # * hacer que se descargue desde el servidor la lista de servidores disponibles
 # * La pagina de sitio denegado tiene que estar en el server, no en el cliente.
-# * Ni bien se cambie la contraseña se deberia notificar.
 # * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
 # * Hacer actualizador para linux.
 # * Internacionalizacion
