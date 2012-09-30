0887aaca42f0a2486ba91180aee1641363f4d580
mboscovich <mboscovich@users.noreply.github.com>
2012-09-30 14:26:03 +0000

    ahora informa la baja de usuarios por mail

--- cliente/branches/release/1.1-rev765/TODO.py
+++ cliente/branches/release/1.1-rev765/TODO.py
@@ -1,8 +1,8 @@
 # * Ni bien se cambie la contraseña se deberia notificar.
+# * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
 # * utilizar HTTPS
 # * hacer que se descargue desde el servidor la lista de servidores disponibles
 # * La pagina de sitio denegado tiene que estar en el server, no en el cliente.
-# * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
 # * Hacer actualizador para linux.
 # * Internacionalizacion
 
