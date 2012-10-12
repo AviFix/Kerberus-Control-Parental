321ac73dba6998e45d8a5996965c375f8fe74865
mboscovich <mboscovich@users.noreply.github.com>
2012-10-12 14:30:49 +0000

    nueva tarea

--- cliente/branches/release/1.1-rev765/TODO.py
+++ cliente/branches/release/1.1-rev765/TODO.py
@@ -1,4 +1,5 @@
+# * Sqlite debe estar encriptado
 # * La pagina de sitio denegado tiene que estar en el server, no en el cliente.
-# * Hacer actualizador para linux.
 # * Internacionalizacion
 # * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
+# * Hacer actualizador para linux y windows.
