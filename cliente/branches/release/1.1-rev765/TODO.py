39c08ee61f00f894668b411854cdbb71d2723a4c
mboscovich <mboscovich@users.noreply.github.com>
2012-10-16 23:59:45 +0000

    typo

--- cliente/branches/release/1.1-rev765/TODO.py
+++ cliente/branches/release/1.1-rev765/TODO.py
@@ -1,5 +1,4 @@
 # * Sqlite debe estar encriptado
-# * La pagina de sitio denegado tiene que estar en el server, no en el cliente.
 # * Internacionalizacion
 # * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
 # * Hacer actualizador para linux y windows.
