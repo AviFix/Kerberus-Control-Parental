f7c9db165b3c7f19e86fb4989a12bd79b142ec16
mboscovich <mboscovich@users.noreply.github.com>
2012-10-19 21:07:50 +0000

    corrijo el autocomplete de la pagina de desbloqueo. Termino el script de migración de la db de 1.0 a 1.1.

--- cliente/branches/release/1.1-rev765/TODO.py
+++ cliente/branches/release/1.1-rev765/TODO.py
@@ -1,8 +1,8 @@
 # Para versión 1.1
-# * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
 # * Hacer actualizador para linux y windows.
+# * Agregar excepciones para todo el acceso a la db
 
 # Para versión 1.2
 # * Sqlite debe estar encriptado
 # * Internacionalizacion
-
+# * Hacer que thunderbird utilice el proxy(al menos en linux no se setea).
--- cliente/branches/release/1.1-rev765/actualizadores/windows/1.0-1.1/migrador.py
+++ cliente/branches/release/1.1-rev765/actualizadores/windows/1.0-1.1/migrador.py
@@ -1,11 +1,19 @@
 import sqlite3
 import hashlib
 
-conexion = sqlite3.connect('kerberus.db')
-cursor = conexion.cursor()
-password = cursor.execute('select password from instalacion;').fetchone()[0]
-credencial = hashlib.md5(password).hexdigest()
-cursor.execute('ALTER TABLE instalacion ADD COLUMN credencial TEXT default %s;' % credencial)
-cursor.execute('ALTER TABLE instalacion ADD COLUMN serverid INTEGER default 1;')
-conexion.commit()
-conexion.close()
+try:
+    conexion = sqlite3.connect('kerberus.db')
+    cursor = conexion.cursor()
+    respuesta = cursor.execute('select password from instalacion;').fetchone()
+    if respuesta:
+        password = respuesta[0]
+        credencial = hashlib.md5(password).hexdigest()
+        cursor.execute('ALTER TABLE instalacion ADD COLUMN credencial TEXT')
+        cursor.execute('ALTER TABLE instalacion ADD COLUMN serverid INTEGER')
+        registro = [(1, credencial, '', '1.1'),]
+        cursor.executemany('update instalacion set serverid=?, credencial=?, '
+                        'password=?, version=?', (registro),)
+        conexion.commit()
+        conexion.close()
+except:
+    pass
