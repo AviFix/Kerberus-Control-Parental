74664dd6ca24eb2665b371591eb5a7af4dc9afc2
mboscovich <mboscovich@users.noreply.github.com>
2012-11-03 21:05:21 +0000

    typo

--- cliente/branches/release/1.1-rev765/instaladores/windows/regenerar-actualizador.bat
+++ cliente/branches/release/1.1-rev765/instaladores/windows/regenerar-actualizador.bat
@@ -1,9 +1,7 @@
-python ../common/pyinstaller-1.5.1/Configure.py
-
 cd migrador
 rmdir /S /Q build
 rmdir /S /Q dist
-python ../../common/pyinstaller-1.5.1/Build.py -y migrador.spec
+python ../../common/pyinstaller-2.0/pyinstaller.py -y --log-level=ERROR migrador.spec
 cd ..
 
 "C:\Archivos de programa\NSIS\makensis.exe" update1.0-1.1-silent.nsi
--- cliente/branches/release/1.1-rev765/instaladores/windows/regenerar-instalador-usuarios-finales.bat
+++ cliente/branches/release/1.1-rev765/instaladores/windows/regenerar-instalador-usuarios-finales.bat
@@ -1,30 +1,28 @@
 copy ..\..\cliente.conf ArchivosDefault\cliente.conf /Y
 copy ..\..\conf\confspec.ini ArchivosDefault\confspec.ini /Y
 
-python ../common/pyinstaller-1.5.1/Configure.py
-
 cd kerberus-daemon
 rmdir /S /Q build
 rmdir /S /Q dist
-python ../../common/pyinstaller-1.5.1/Build.py -y cliente.spec
+python ../../common/pyinstaller-2.0/pyinstaller.py -y --log-level=ERROR cliente.spec
 cd ..
 
 cd kerberus-sync
 rmdir /S /Q build
 rmdir /S /Q dist
-python ../../common/pyinstaller-1.5.1/Build.py -y sincronizadorCliente.spec
+python ../../common/pyinstaller-2.0/pyinstaller.py -y --log-level=ERROR sincronizadorCliente.spec
 cd ..
 
 cd Navegadores
 rmdir /S /Q build
 rmdir /S /Q dist
-python ../../common/pyinstaller-1.5.1/Build.py -y navegadores.spec
+python ../../common/pyinstaller-2.0/pyinstaller.py -y --log-level=ERROR navegadores.spec
 cd ..
 
 cd desinstalador
 rmdir /S /Q build
 rmdir /S /Q dist
-python ../../common/pyinstaller-1.5.1/Build.py -y desinstalador.spec
+python ../../common/pyinstaller-2.0/pyinstaller.py -y --log-level=ERROR desinstalador.spec
 cd ..
 
 "C:\Archivos de programa\NSIS\makensis.exe" kerberus-usuarios-finales.nsi
