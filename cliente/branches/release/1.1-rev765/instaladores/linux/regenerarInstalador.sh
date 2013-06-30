45a46704f2bfe70c7c6d37b2d3f4dc68b90258d2
mboscovich <mboscovich@users.noreply.github.com>
2013-06-30 13:51:24 +0000

    typo

--- cliente/branches/release/1.1-rev765/instaladores/linux/regenerarInstalador.sh
+++ cliente/branches/release/1.1-rev765/instaladores/linux/regenerarInstalador.sh
@@ -11,7 +11,7 @@ else
 	ARQUITECTURA="32bits"
 fi
 
-nombre_inst="kerberus-installer-${VERSION}-${ARQUITECTURA}.sh"
+nombre_inst="kerberus-installer-${VERSION}-${ARQUITECTURA}"
 
 grep "ENTORNO_DE_DESARROLLO = True" ../../conf/config.py > /dev/null
 if [ ${?} -eq 0 ]; then 
